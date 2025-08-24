754ae815c332937664a916af58c98dfdf07e0170
MnqobiNkabinde <mnqobi.nkabinde@bbd.co.za>
2025-08-24 13:12:34 +0200

    bruh

--- backend/services/aiService.js
+++ backend/services/aiService.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 
-const token = "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";
+const token = process.env.REACT_APP_SERVICE_TOKEN;
 const endpoint = "https://models.github.ai/inference";
 const model = "gpt-4o";
 
